e4d9d635a1e3559556cfe049d9efac3ada071340
Malloumlaya <dev@malloum.fr>
2014-03-05 14:05:25 +0100

    refactoring after a brainstorming with @dadoonet

---
+++ src/main/java/org/scrutmydocs/datasource/SMDDataSourcesManager.java
@@ -0,0 +1,41 @@
+package org.scrutmydocs.datasource;
+
+import java.util.List;
+
+import org.scrutmydocs.contract.SMDDataSource;
+
+public class SMDDataSourcesManager {
+
+	/**
+	 * return a collection of Configuration use in the Scrutmydocs instance
+	 * 
+	 * @return
+	 */
+	public static List<SMDDataSource> getListDataSources() {
+
+		return null;
+	}
+
+	/**
+	 * update a collection of Configuration use in the Scrutmydocs instance
+	 * 
+	 * @return
+	 */
+	public static void updateListConfigurations(
+			List<SMDDataSource> Configurations) {
+
+		// don't forget to check if name and id was a unique key
+
+	}
+
+	/**
+	 * return Configuration from name and id
+	 * 
+	 * @return
+	 */
+	public static SMDDataSource getConfiguration(String name, String id) {
+
+		return null;
+	}
+
+}
