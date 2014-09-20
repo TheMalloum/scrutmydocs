e32d81323be83c88fe039ad6ca83a6de6808e6ea
Malloum Laya <malloum.laya@gmail.com>
2014-09-21 00:07:58 +0200

    CONTINU

--- src/main/java/org/scrutmydocs/servlet/ServletInit.java
+++ src/main/java/org/scrutmydocs/servlet/ServletInit.java
@@ -1,8 +1,11 @@
 package org.scrutmydocs.servlet;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 
 import javax.servlet.ServletConfig;
@@ -18,7 +21,7 @@ public class ServletInit extends HttpServlet {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -3093542409787735469L;
-	
+
 	protected Logger logger = Logger.getLogger(getClass().getName());
 
 	@Override
@@ -26,27 +29,60 @@ public class ServletInit extends HttpServlet {
 		super.init(config);
 
 		try {
-			
-			logger.info("index NOTICE AND LICENCE ....");
-	        
-			SMDDocument LICENCE = new SMDDocument(new File(getClass().getClassLoader().getResource("LICENCE").toURI()));
-			SMDDocument NOTICE = new SMDDocument(new File(getClass().getClassLoader().getResource("NOTICE").toURI()));
 
-			SMDSearchFactory.getInstance().index(new UploadSMDPlugin(), LICENCE);
+			logger.info("index NOTICE AND LICENCE ....");
+
+			SMDDocument LICENCE = new SMDDocument(new File(getClass()
+					.getClassLoader().getResource("LICENCE").toURI()));
+			SMDDocument NOTICE = new SMDDocument(new File(getClass()
+					.getClassLoader().getResource("NOTICE").toURI()));
+
+			SMDSearchFactory.getInstance()
+					.index(new UploadSMDPlugin(), LICENCE);
 			SMDSearchFactory.getInstance().index(new UploadSMDPlugin(), NOTICE);
 
-			
 			logger.info("index NOTICE AND LICENCE .... OK");
 
 		} catch (Exception e) {
-			logger.error("NOTICE AND LICENCE can't be index",e);
+			logger.error("NOTICE AND LICENCE can't be index", e);
 		}
 	}
-	
-	public static void main(String[] args) throws ServletException, FileNotFoundException, IOException, URISyntaxException {
-		
-		
-		SMDDocument LICENCE = new SMDDocument(new File(ClassLoader.getSystemResource("LICENCE").toURI()));
+
+//	public InputStream getResourceAsStream(String name) {
+//		name = resolveName(name);
+//		ClassLoader cl = getClassLoader0();
+//		if (cl == null) {
+//			// A system class.
+//			return ClassLoader.getSystemResourceAsStream(name);
+//		}
+//		return cl.getResourceAsStream(name);
+//	}
+
+	public String readFileInClasspath(String url) throws Exception {
+		StringBuffer bufferJSON = new StringBuffer();
+
+		try {
+			InputStream ips = ClassLoader.getSystemResourceAsStream(url);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			String line;
+
+			while ((line = br.readLine()) != null) {
+				bufferJSON.append(line);
+			}
+			br.close();
+		} catch (Exception e) {
+			return null;
+		}
+
+		return bufferJSON.toString();
+	}
+
+	public static void main(String[] args) throws ServletException,
+			FileNotFoundException, IOException, URISyntaxException {
+
+		SMDDocument LICENCE = new SMDDocument(new File(ClassLoader
+				.getSystemResource("LICENCE").toURI()));
 		System.err.println(LICENCE);
 
 	}
