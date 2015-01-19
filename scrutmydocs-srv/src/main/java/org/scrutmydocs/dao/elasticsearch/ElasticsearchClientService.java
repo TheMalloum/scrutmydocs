68f66453d20c8ecf7eff91bf014ef92ebbabb478
David Pilato <david@pilato.fr>
2015-01-19 10:07:42 +0100

    Use elasticsearch-beyonder project
    This project automatically creates elasticsearch object when starting.

--- scrutmydocs-srv/src/main/java/org/scrutmydocs/dao/elasticsearch/ElasticsearchClientService.java
+++ scrutmydocs-srv/src/main/java/org/scrutmydocs/dao/elasticsearch/ElasticsearchClientService.java
@@ -19,13 +19,10 @@
 
 package org.scrutmydocs.dao.elasticsearch;
 
-import com.google.common.io.Files;
-import org.apache.commons.io.IOUtils;
-import org.elasticsearch.action.admin.indices.mapping.get.GetMappingsResponse;
+import fr.pilato.elasticsearch.tools.ElasticsearchBeyonder;
+import fr.pilato.elasticsearch.tools.index.IndexFinder;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.ImmutableSettings;
-import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.indices.IndexMissingException;
 import org.scrutmydocs.domain.SMDConfiguration;
@@ -36,9 +33,7 @@ import restx.factory.AutoStartable;
 import restx.factory.Component;
 
 import javax.inject.Inject;
-import java.io.*;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
+import java.io.IOException;
 import java.util.Collection;
 
 @Component
@@ -47,10 +42,6 @@ public class ElasticsearchClientService implements AutoStartable {
     private static final Logger logger = LoggerFactory.getLogger(ElasticsearchClientService.class);
 
     private static final String templateDirName = "estemplate";
-    private static final String jsonExtension = ".json";
-    private static final String indexSettingsFileName = "_settings" + jsonExtension;
-    private static final File templateDir = new File(ElasticsearchService.class.getClassLoader()
-            .getResource(templateDirName).getFile());
 
     private final Client esClient;
     private final SMDConfiguration smdConfiguration;
@@ -86,70 +77,31 @@ public class ElasticsearchClientService implements AutoStartable {
     public void start() {
         logger.debug("Client Node started. Checking elasticsearch config...");
 
-        // Let's create automatically indices and mappings
-        Collection<String> indices = getExistingIndices();
-
-        // For each we create it if needed
-        for (String index : indices) {
-            String indexName = Files.getNameWithoutExtension(index);
-            logger.debug("looking for index [{}]", indexName);
-
-            if (smdConfiguration.isCleanOnStart()) {
-                logger.warn("/!\\ Clean On Start activated /!\\. Removing index [{}]", indexName);
-                try {
-                    esClient.admin().indices().prepareDelete(indexName).get();
-                } catch (IndexMissingException e) {
-                    // We ignore if the index was not existing before
-                }
-            }
-
-            // If index does not exist, let's create it
-            boolean indexExists = esClient.admin().indices().prepareExists(indexName).get().isExists();
-            if (indexExists) {
-                logger.debug("Index [{}] already exists. Skipping.", indexName);
-            } else {
-                File settings = getExistingSettings(indexName);
-                if (settings != null) {
+        // Let's remove automatically indices and mappings if CleanOnStart is set
+        if (smdConfiguration.isCleanOnStart()) {
+            try {
+                Collection<String> indices = IndexFinder.findIndexNames(templateDirName);
+                // For each we remove it if needed
+                for (String index : indices) {
+                    logger.warn("/!\\ Clean On Start activated /!\\. Removing index [{}]", index);
                     try {
-                        Settings indexSettings = ImmutableSettings.builder()
-                                .loadFromUrl(settings.toPath().toUri().toURL())
-                                .build();
-                        logger.trace("Settings for [{}]: [{}]", indexName, indexSettings.toDelimitedString(','));
-
-                        esClient.admin().indices().prepareCreate(indexName).setSettings(indexSettings).get();
-                        logger.debug("Index [{}] created.", indexName);
-                    } catch (MalformedURLException e) {
-                        logger.warn("Can not read settings from [{}]", settings);
+                        esClient.admin().indices().prepareDelete(index).get();
+                    } catch (IndexMissingException e) {
+                        // We ignore if the index was not existing before
                     }
                 }
-            }
-
-            Collection<String> types = getExistingMappings(index);
-            for (String type : types) {
-                String mappingName = Files.getNameWithoutExtension(type);
-                logger.debug("looking for mapping [{}/{}]", indexName, mappingName);
-
-                // If type does not exist, let's create it
-                GetMappingsResponse response = esClient.admin().indices()
-                        .prepareGetMappings(indexName).addTypes(mappingName).get();
-                boolean mappingExists = (response.getMappings().containsKey(indexName) &&
-                        response.getMappings().get(indexName).containsKey(mappingName));
-                if (mappingExists) {
-                    logger.debug("Type [{}/{}] already exists. Skipping.", indexName, mappingName);
-                } else {
-                    String mapping = getExistingMapping(indexName, mappingName);
-                    if (mapping != null) {
-                        logger.trace("Mappings for [{}/{}]: [{}]", indexName, mappingName, mapping);
-                        esClient.admin().indices().preparePutMapping(indexName)
-                            .setType(mappingName).setSource(mapping).get();
-                        logger.debug("Mapping [{}/{}] created.", indexName, mappingName);
-                    }
-                }
+            } catch (IOException e) {
+                logger.warn("Can not read [{}]...", templateDirName);
             }
         }
 
-        logger.info("Elasticsearch config checked... Rock & roll!");
+        try {
+            ElasticsearchBeyonder.start(esClient, templateDirName);
+        } catch (Exception e) {
+            logger.error("Can not setup elasticsearch", e);
+        }
 
+        logger.info("Elasticsearch config checked... Rock & roll!");
     }
 
     public Client getEsClient() {
@@ -171,105 +123,5 @@ public class ElasticsearchClientService implements AutoStartable {
         }
 
         return new InetSocketTransportAddress(splitted[0], port);
-    }
-
-    private static Collection<String> getExistingIndices() {
-        logger.trace("Reading dirs in [{}]...", templateDirName);
-        Collection<String> indices = new ArrayList<>();
-        if (!templateDir.exists()) {
-            logger.debug("[{}] dir does not exist...", templateDirName);
-            return indices;
-        }
-        if (!templateDir.isDirectory()) {
-            logger.warn("[{}] is not a directory...", templateDirName);
-            return indices;
-        }
-        File[] files = templateDir.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                logger.trace("added [{}]...", file);
-                indices.add(file.getName());
-            } else {
-                logger.trace("ignoring [{}]...", file);
-            }
-        }
-
-        return indices;
-    }
-
-    private static Collection<String> getExistingMappings(String index) {
-        logger.trace("Reading mappings in [{}]...", index);
-        Collection<String> mappings = new ArrayList<>();
-        File dir = new File(templateDir, index);
-        if (!dir.exists()) {
-            logger.debug("[{}] dir does not exist...", index);
-            return mappings;
-        }
-        if (!dir.isDirectory()) {
-            logger.warn("[{}] is not a directory...", index);
-            return mappings;
-        }
-        File[] files = dir.listFiles();
-        for (File file : files) {
-            if (!file.isDirectory() && !file.getName().equals(indexSettingsFileName)) {
-                logger.trace("added [{}]...", file);
-                mappings.add(file.getName());
-            } else {
-                logger.trace("ignoring [{}]...", file);
-            }
-        }
-
-        return mappings;
-    }
-
-    private static File getExistingSettings(String index) {
-        logger.trace("Reading settings for [{}]...", index);
-        File dir = new File(templateDir, index);
-        File settings = new File(dir, indexSettingsFileName);
-        if (!settings.exists()) {
-            logger.debug("[{}/{}] does not exist at [{}].", index, indexSettingsFileName, settings);
-            return null;
-        }
-        if (!settings.isFile()) {
-            logger.warn("[{}/{}] is not a file at [{}]!", index, indexSettingsFileName, settings);
-            return null;
-        }
-
-        return settings;
-    }
-
-    /**
-     * Read a mapping
-     * @param index index name
-     * @param type type name (.json will be appended)
-     * @return
-     */
-    private static String getExistingMapping(String index, String type) {
-        logger.trace("Reading mapping for [{}/{}]...", index, type);
-        File dir = new File(templateDir, index);
-        File settings = new File(dir, type + jsonExtension);
-        if (!settings.exists()) {
-            logger.debug("[{}/{}] does not exist at [{}].", index, type, settings);
-            return null;
-        }
-        if (!settings.isFile()) {
-            logger.warn("[{}/{}] is not a file at [{}]!", index, type, settings);
-            return null;
-        }
-
-        String mapping = null;
-
-        logger.trace("Reading file [{}]...", settings);
-        try (InputStream asStream = new FileInputStream(settings)) {
-            if (asStream == null) {
-                logger.warn("Can not find [{}] in class loader.", settings);
-                return null;
-            }
-            mapping = IOUtils.toString(asStream);
-        } catch (IOException e) {
-            logger.warn("Can not read [{}].", settings);
-        }
-
-        return mapping;
     }
 }
